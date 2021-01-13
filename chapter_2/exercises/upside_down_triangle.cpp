e30baffce3523b4b4cc7942f72c8a4a0c99ddc1e
Nick <Land>
2021-01-13 15:27:12 -0500

    ✨ Exercise 2-1 complete

--- chapter_2/exercises/upside_down_triangle.cpp
+++ chapter_2/exercises/upside_down_triangle.cpp
@@ -1,10 +1,25 @@
+// Think Like A Programmer Exercise 2-1
+//
+// Write a program using only single-character output statements that
+// output a hash mark, a space, or an end-of-line.
+//
+// Write a program that produces the following shape:
+//
+// ########
+//  ######
+//   ####
+//    ##
+
 #include <iostream>
 using std::cin;
 using std::cout;
 
 int main() {
-  for(int column = 0; column < 4; column++) {
-    for(int row = 0; row < 8; row++) {
+  for(int row = 1; row <= 4; row++) {
+    for(int space = 0; space < (row - 1); space++) {
+      cout << " ";
+    }
+    for(int hash = 0; hash < 8 - (row - 1) * 2; hash++) {
       cout << "#";
     }
     cout << "\n";
