194684611131f4fa670be2cc9e99ce3129de74ca
Nick <Land>
2021-01-15 14:34:25 -0500

    ✨ Exercise 2-2 done
    This is a pretty brute force solution. I'll look to refactor.

---
+++ chapter_2/exercises/exercise_2-2.cpp
@@ -0,0 +1,44 @@
+// Think Like A Programmer Exercise 2-2
+//
+// Write a program using only single-character output statements that
+// output a hash mark, a space, or an end-of-line.
+//
+// Write a program that produces the following shape:
+//
+//    ##
+//   ####
+//  ######
+// ########
+// ########
+//  ######
+//   ####
+//    ##
+
+#include <iostream>
+using std::cin;
+using std::cout;
+
+int main() {
+  // Upper half
+  for(int row = 4; row >= 1; row--) {
+    for(int space = 0; space < (row - 1); space++) {
+      cout << " ";
+    }
+    for(int hash = 0; hash < 8 - (row - 1) * 2; hash++) {
+      cout << "#";
+    }
+    cout << "\n";
+  }
+
+  // Lower half
+  for(int row = 1; row <= 4; row++) {
+    for(int space = 0; space < (row - 1); space++) {
+      cout << " ";
+    }
+    for(int hash = 0; hash < 8 - (row - 1) * 2; hash++) {
+      cout << "#";
+    }
+    cout << "\n";
+  }
+  return 0;
+}
