9d5e74b6ccbde84fd360d95d1e7435f26be1b060
Nick <Land>
2021-01-19 13:10:01 -0500

    🔨 Refactor solution for Fining The Mode
    This is a simpler approach to what was done before. The code could be
    considered a little harder to read now, but the solution is a little
    more flexible.

--- chapter_3/finding_the_mode.cpp
+++ chapter_3/finding_the_mode.cpp
@@ -12,33 +12,27 @@
 using std::cin;
 using std::cout;
 
-int compareFunc(int, int);
-
 int main() {
   const int ARRAY_SIZE = 12;
   int surveyData[ARRAY_SIZE] = {4, 7, 3, 8, 9, 7, 3, 9, 9, 3, 3, 10};
 
-  int mostFrequent;
-  int highestFrequency = 0;
-  int currentFrequency = 0;
+  cout << "Finding the mode of: " << "{4, 7, 3, 8, 9, 7, 3, 9, 9, 3, 3, 10}\n";
 
-  qsort(surveyData, ARRAY_SIZE, sizeof(int), compareFunc);
+  const int MAX_RESPONSE = 10;
+  int histogram[MAX_RESPONSE];
+  for(int i = 0; i < MAX_RESPONSE; i++) {
+    histogram[i] = 0;
+  }
 
   for(int i = 0; i < ARRAY_SIZE; i++) {
-    currentFrequency++;
-    // if (surveydata[i] IS LAST OCCURANCE OF A VALUE)
-    if(i == ARRAY_SIZE - 1 || surveyData[i] != surveyData[i + 1]) {
-      if(currentFrequency > highestFrequency) {
-	highestFrequency = currentFrequency;
-	mostFrequent = surveyData[i];
-      }
-      currentFrequency = 0;
-    }
+    histogram[surveyData[i] - 1]++;
   }
-}
 
-int compareFunc(const void *voidA, const void *voidB) {
-  int *intA = (int *) (voidA);
-  int *intB = (int *) (voidB);
-  return *intA - *intB;
+  int mostFrequent = 0;
+  for(int i = 1; i < MAX_RESPONSE; i++) {
+    if(histogram[i] > histogram[mostFrequent]) mostFrequent = i;
+  }
+  mostFrequent++;
+
+  cout << "The mode is: " << mostFrequent;
 }
