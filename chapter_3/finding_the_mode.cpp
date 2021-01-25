051881b5cdc76778b3110c20900a8260c835eba1
Nick <Land>
2021-01-25 15:38:54 -0500

    🎨 Reformat according to lsp-mode
    I tried out a feature of lsp-mode in Emacs, and this is the formatting
    result.

--- chapter_3/finding_the_mode.cpp
+++ chapter_3/finding_the_mode.cpp
@@ -1,5 +1,5 @@
 // Think Like A Programmer
-// 
+//
 // Problem: Finding The Mode
 //
 // In statistics, the mode of a set of values is the value that
@@ -9,28 +9,29 @@
 // set. For our purpose, if multiple modes exist, any may be chosen.
 
 #include <iostream>
-using std::cin;
 using std::cout;
 
 int main() {
   const int ARRAY_SIZE = 12;
   int surveyData[ARRAY_SIZE] = {4, 7, 3, 8, 9, 7, 3, 9, 9, 3, 3, 10};
 
-  cout << "Finding the mode of: " << "{4, 7, 3, 8, 9, 7, 3, 9, 9, 3, 3, 10}\n";
+  cout << "Finding the mode of: "
+       << "{4, 7, 3, 8, 9, 7, 3, 9, 9, 3, 3, 10}\n";
 
   const int MAX_RESPONSE = 10;
   int histogram[MAX_RESPONSE];
-  for(int i = 0; i < MAX_RESPONSE; i++) {
+  for (int i = 0; i < MAX_RESPONSE; i++) {
     histogram[i] = 0;
   }
 
-  for(int i = 0; i < ARRAY_SIZE; i++) {
+  for (int i = 0; i < ARRAY_SIZE; i++) {
     histogram[surveyData[i] - 1]++;
   }
 
   int mostFrequent = 0;
-  for(int i = 1; i < MAX_RESPONSE; i++) {
-    if(histogram[i] > histogram[mostFrequent]) mostFrequent = i;
+  for (int i = 1; i < MAX_RESPONSE; i++) {
+    if (histogram[i] > histogram[mostFrequent])
+      mostFrequent = i;
   }
   mostFrequent++;
 
